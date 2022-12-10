9cd89b830c98ee9fe7671c69e2e7da5b60e6ed68
constROD <rialcantara21@gmail.com>
2022-12-10 21:05:43 +0800

    chore: add validator

---
+++ src/shared/utils/Validator.ts
@@ -0,0 +1,23 @@
+import { IValidatorError, IValidatorResponse } from 'shared/types/Validator';
+import * as yup from 'yup';
+
+export const validate = <T>(
+  schema: yup.ObjectSchema<any>,
+  values: T
+): Promise<IValidatorResponse<T>> =>
+  new Promise(resolve => {
+    schema
+      .validate(values, { abortEarly: false, stripUnknown: true })
+      .then(data => {
+        const sanitizedData =
+          Object.keys(data as Record<string, unknown>).length !== 0 ? (data as T) : undefined;
+        resolve({ data: sanitizedData, error: undefined });
+      })
+      .catch(err => {
+        const error = err.inner.map((e: { path: string; message: string }) => ({
+          id: e.path,
+          message: e.message,
+        })) as IValidatorError[];
+        resolve({ data: undefined, error });
+      });
+  });
